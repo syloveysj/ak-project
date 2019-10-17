60d809436834ca0676881c1bd2e0acb7d4cfdf4f
jay <478233446@qq.com>
2019-10-17 16:55:03 +0800

    初始化

--- ak-lib-kong/src/main/java/com/yunjian/ak/kong/client/model/admin/target/Target.java
+++ ak-lib-kong/src/main/java/com/yunjian/ak/kong/client/model/admin/target/Target.java
@@ -1,10 +1,7 @@
 package com.yunjian.ak.kong.client.model.admin.target;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
-
-import java.util.Date;
 
 /**
  * Created by vaibhav on 14/06/17.
