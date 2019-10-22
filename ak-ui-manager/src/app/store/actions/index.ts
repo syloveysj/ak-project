9b2b6daeff475258a7ddaf743329144ea9ab0863
jay <478233446@qq.com>
2019-10-23 01:11:28 +0800

    初始化

--- ak-ui-manager/src/app/store/actions/index.ts
+++ ak-ui-manager/src/app/store/actions/index.ts
@@ -1,7 +1,9 @@
 import * as RouterActions from '@store/actions/router.actions';
 import * as ConstantsActions from '@store/actions/constants.actions';
+import * as AuthActions from '@store/actions/auth.actions';
 
 export {
     RouterActions,
-    ConstantsActions
+    ConstantsActions,
+    AuthActions
 };
