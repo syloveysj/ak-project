1a02442e1d5be23521926b785cfbc3b49d8b3382
jay <478233446@qq.com>
2019-10-24 17:18:12 +0800

    初始化

--- ak-service-gateway/src/main/java/com/yunjian/ak/gateway/controller/apis/ApisController.java
+++ ak-service-gateway/src/main/java/com/yunjian/ak/gateway/controller/apis/ApisController.java
@@ -4,10 +4,18 @@ import com.alibaba.fastjson.JSON;
 import com.yunjian.ak.exception.ValidationException;
 import com.yunjian.ak.gateway.controller.router.UpstreamController;
 import com.yunjian.ak.gateway.entity.apis.ServiceEntity;
+import com.yunjian.ak.gateway.entity.apis.UpstreamEntity;
 import com.yunjian.ak.gateway.service.apis.ApisServiceService;
+import com.yunjian.ak.gateway.service.apis.ApisUpstreamService;
 import com.yunjian.ak.gateway.vo.apis.ApiAnalysisVo;
 import com.yunjian.ak.gateway.vo.apis.ApiPackageVo;
 import com.yunjian.ak.gateway.vo.apis.ApiVo;
+import com.yunjian.ak.kong.client.impl.KongClient;
+import com.yunjian.ak.kong.client.model.admin.route.RouteList;
+import com.yunjian.ak.kong.client.model.admin.service.Service;
+import com.yunjian.ak.kong.client.model.admin.target.TargetList;
+import com.yunjian.ak.kong.client.model.admin.upstream.Upstream;
+import com.yunjian.ak.utils.UUIDUtil;
 import com.yunjian.ak.web.utils.http.RestUtils;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -16,6 +24,7 @@ import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,11 +51,106 @@ public class ApisController {
     @Autowired
     private ApisServiceService apisServiceService;
 
+    @Autowired
+    private ApisUpstreamService apisUpstreamService;
+
+    @Autowired
+    private KongClient kongApisClient;
+
+    @PostMapping("/services")
+    @ApiOperation("添加Services")
+    @ApiResponses({@ApiResponse(
+            code = 200,
+            message = "添加Services成功",
+            response = ServiceEntity.class
+    )})
+    public ServiceEntity insert(@Valid @RequestBody ServiceEntity entity) {
+        LOGGER.debug("请求 ApisController 的 Services insert!");
+
+        // 调用接口添加上游
+        Upstream upstream = new Upstream();
+        upstream.setName(UUIDUtil.createUUID());
+        upstream.setSlots(10000);
+        upstream = kongApisClient.getUpstreamService().createUpstream(upstream);
+
+        // 调用接口添加服务
+        Service service = new Service();
+        service.setName(UUIDUtil.createUUID());
+        service.setProtocol("http");
+        service.setHost(upstream.getName());
+        service.setPort(80);
+        service.setConnectTimeout(60000L);
+        service.setWriteTimeout(60000L);
+        service.setReadTimeout(60000L);
+        service = kongApisClient.getServiceService().createService(service);
+
+        // 通过数据库更新相关信息
+        UpstreamEntity upstreamEntity = new UpstreamEntity();
+        upstreamEntity.setId(upstream.getId());
+        upstreamEntity.setAlias(entity.getAlias());
+        apisUpstreamService.update(upstreamEntity);
+
+        entity.setId(service.getId());
+        apisServiceService.update(entity);
+        return null;
+    }
+
+    @PutMapping("/services/{id}")
+    @ApiOperation("更新Services")
+    @ApiResponses({@ApiResponse(
+            code = 200,
+            message = "添加Services成功",
+            response = ServiceEntity.class
+    )})
+    public ServiceEntity update(@Valid @RequestBody ServiceEntity entity) {
+        LOGGER.debug("请求 ApisController 的 Services update!");
+
+        // 调用接口添加服务
+        Service service = new Service();
+        BeanUtils.copyProperties(entity, service);
+        service = kongApisClient.getServiceService().updateService(entity.getId(), service);
+
+        // 通过数据库更新相关信息
+        UpstreamEntity upstreamEntity = new UpstreamEntity();
+        upstreamEntity.setName(entity.getHost());
+        upstreamEntity.setAlias(entity.getAlias());
+        apisUpstreamService.update(upstreamEntity);
+
+        entity.setId(service.getId());
+        apisServiceService.update(entity);
+        return entity;
+    }
+
+    @DeleteMapping("/services/{id}")
+    @ApiOperation("删除指定id的Services")
+    @ApiResponses({@ApiResponse(
+            code = 200,
+            message = "删除指定id的Services成功"
+    )})
+    public void delete(@PathVariable("id") String id) {
+        LOGGER.debug("请求 ApisController 删除指定id的Services:{}!", id);
+
+        ServiceEntity entity = apisServiceService.get(id);
+
+        TargetList targetList = kongApisClient.getTargetService().listTargets(entity.getHost(), null, null, null, null, null);
+        if(targetList.getData() != null && targetList.getData().size() > 0) {
+            throw new ValidationException("请先删除目标Targets");
+        }
+
+        RouteList routeList = kongApisClient.getRouteService().listRoutesByService(entity.getId(), null, null, null, 2L, null);
+        if(routeList.getData() != null && routeList.getData().size() > 0) {
+            throw new ValidationException("请先删除路由Routes");
+        }
+
+        kongApisClient.getServiceService().deleteService(entity.getId());
+        kongApisClient.getUpstreamService().deleteUpstream(entity.getHost());
+    }
+
     @GetMapping("/services")
     @ApiOperation("获取所有Services")
     @ApiResponses({@ApiResponse(
             code = 200,
-            message = "获取所有ApplicationType成功",
+            message = "获取所有Services成功",
             response = ServiceEntity.class,
             responseContainer = "List"
     )})
