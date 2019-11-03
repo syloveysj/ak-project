6954c6edf0ab2cca267e98a8153e4f6694a55329
jay <478233446@qq.com>
2019-11-03 22:37:10 +0800

    初始化

--- ak-lib-api-proxy/src/main/java/com/yunjian/ak/api/proxy/ProxyController.java
+++ ak-lib-api-proxy/src/main/java/com/yunjian/ak/api/proxy/ProxyController.java
@@ -14,7 +14,6 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
